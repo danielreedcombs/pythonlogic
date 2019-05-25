5b808a34d4054aee3d91eacecfc4efbaa2cef647
Daniel Combs <danielreedcombs@gmail.com>
2019-05-25 08:42:19 -0500

    grill working

--- pythonpractice.py
+++ pythonpractice.py
@@ -1,5 +1,6 @@
 nums = [1,2,3,4,5,6,7,8,9]
 words = ["ruthy", "daniel", "drexel", "meira", "zach", "will", "nate"]
+meats=["chicken", "cow", "pig", "duck"]
 
 def primefunction(nums):
     for num in nums:
@@ -27,3 +28,20 @@ def friendsbackwards(words):
         print(container)
 
 friendsbackwards(words)
+
+def grill(meat):
+    grilledmeat = ["chicken", "burger", "bacon", "duck"]
+    if meat == "chicken":
+        print(f"order up! you ordered {grilledmeat[0]}")
+    elif meat == "cow":
+        print(f"order up! you ordered {grilledmeat[1]}")
+    elif meat == "pig":
+        print(f"order up! you ordered {grilledmeat[2]}")
+    elif meat == "duck":
+        print(f"order up! you ordered {grilledmeat[3]}")
+
+def bbq(meats):
+    for meat in meats:
+        grill(meat)
+
+bbq(meats)
