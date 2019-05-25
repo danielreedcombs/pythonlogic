f2c728f0dd0ab4fac45cb40060c1f79245405604
Daniel Combs <danielreedcombs@gmail.com>
2019-05-25 08:59:33 -0500

    dog walkers created

--- pythonpractice.py
+++ pythonpractice.py
@@ -1,7 +1,10 @@
 nums = [1,2,3,4,5,6,7,8,9]
 words = ["ruthy", "daniel", "drexel", "meira", "zach", "will", "nate"]
 meats=["chicken", "cow", "pig", "duck"]
-
+luna = ["luna", "not walked", "hungry"]
+frogdog = ["frog dog", "not walked", "hungry"]
+frogpuppy = ["frog puppy", "not walked", "hungry"]
+dogs= [luna, frogdog, frogpuppy]
 def primefunction(nums):
     for num in nums:
         if num < 5:
@@ -45,3 +48,30 @@ def bbq(meats):
         grill(meat)
 
 bbq(meats)
+
+def dogwalker(puppy):
+    if puppy[1] == "not walked":
+        puppy[1] = "walked"
+        print(f"{puppy[0]} has been walked")
+    else:
+        print(f"this dog has been walked")
+
+dogwalker(luna)
+
+def dogfeeder(puppy):
+    if puppy[2] == "hungry":
+        puppy[2] = "full puppy tummy!"
+        print(f"{puppy[0]} has a {puppy[2]}")
+    else:
+        print (f"{puppy[0]} is not hungry")
+
+def great_dog_caretaker(puppy):
+    dogwalker(puppy)
+    dogfeeder(puppy)
+
+great_dog_caretaker(frogdog)
+
+def okay_dog_caretaker(puppy):
+    dogwalker(puppy)
+
+okay_dog_caretaker(frogpuppy)
